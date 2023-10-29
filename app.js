05cd13f34651666e848372d7520dae3191a08516
B Nitish Durga <137098123+nitishbytaru@users.noreply.github.com>
2023-10-29 23:38:17 +0530

    Update app.js
    add many features like success alert storing the data by user

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ let loginButton = document.getElementById("loginButton");
 let registerButton = document.getElementById("registerButton");
 let displayLogin = document.getElementById("displayLogin");
 let displayRegister = document.getElementById("displayRegister");
+let successAlert=document.getElementById("successAlert");
+let incorrectDetails=document.getElementById("incorrectDetails");
 
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let loginDataButton = document.getElementById("loginDataButton");
 
-loginButton.addEventListener("click",function(){
-    displayRegister.style.display="none";
-    displayLogin.style.display="block";
+let newUsername = document.getElementById("newUsername");
+let newPassword = document.getElementById("newPassword");
+let email = document.getElementById("email");
+let registerDataButton = document.getElementById("registerDataButton");
+
+loginDataButton.addEventListener("click",function (){
+    incorrectDetails.style.display="block";
 });
 
-registerButton.addEventListener("click",function(){
-    displayLogin.style.display="none";
-    displayRegister.style.display="block";
+registerDataButton.addEventListener("click",function (){
+    successAlert.style.display="block";
+});
+
+loginButton.addEventListener("click", function () {
+  displayRegister.style.display = "none";
+  displayLogin.style.display = "block";
+});
+
+registerButton.addEventListener("click", function () {
+  displayLogin.style.display = "none";
+  displayRegister.style.display = "block";
 });
